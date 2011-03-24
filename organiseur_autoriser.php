aef746e36445a74f1b8b1446992e200982f883dd
kent1 <kent1@arscenic.info>
2011-03-24 09:28:00 +0000

    Ne pas afficher les boutons de messagerie et d'agenda si on les a désactivé dans la conf

--- organiseur_autoriser.php
+++ organiseur_autoriser.php
@@ -17,3 +17,15 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @return void
  */
 function organiseur_autoriser(){}
+
+function autoriser_calendrier_bouton_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
+	if($GLOBALS['meta']['messagerie_agenda'] == 'oui')
+		return true;
+	return false;
+}
+
+function autoriser_messagerie_bouton_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
+	if($GLOBALS['meta']['messagerie_agenda'] == 'oui')
+		return true;
+	return false;
+}
