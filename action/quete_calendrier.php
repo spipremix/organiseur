8a0d9ce5c10922de9faac34ca896a17ac8e269ae
Cerdic <cedric@yterium.com>
2011-04-03 22:02:58 +0000

    modernisons le calendrier avec la librairie fullcalendar pour afficher en lieu et place de inc/agenda. La liste des evenements est requetee en json via quete_calendrier()
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/organiseur@46322 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ action/quete_calendrier.php
@@ -0,0 +1,79 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2011                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function convert_dateical($dateical){
+	$d = explode('T',$dateical);
+	$amj = reset($d);
+	$s = substr($amj,0,4).'-'.substr($amj,4,2).'-'.substr($amj,6,2);
+	if (count($d)>1){
+		$his = end($d);
+		$s .= ' '.substr($his,0,2).":".substr($his,2,2).":".substr($his,4,2);
+	}
+	return $s;
+}
+function action_quete_calendrier_dist(){
+	$securiser_action = charger_fonction('securiser_action','inc');
+	$securiser_action();
+
+	$start = _request('start');
+	$end = _request('end');
+
+	include_spip('inc/quete_calendrier');
+
+	// recuperer la liste des evenements au format ics
+	$limites = array(sql_quote(date('Y-m-d H:i:s',$start)),sql_quote(date('Y-m-d H:i:s',$end)));
+	list($entier,$duree) = quete_calendrier_interval($limites);
+
+	// la retransformer au format attendu par fullcalendar
+	$evt = array();
+	// facile : chaque evt n'est mentionne qu'une fois, a une date
+	foreach($entier as $amj=>$l){
+		$date = substr($amj,0,4).'-'.substr($amj,4,2).'-'.substr($amj,6,2);
+		foreach($l as $e){
+			$evt[] = array(
+				'id' => 0,
+				'title' => $e['SUMMARY'],
+				'allDay' => true,
+				'start' => $date,
+				'end' => $date,
+				'url' => str_replace('&amp;','&',$e['URL']),
+				'className' => $e['CATEGORIES'],
+				'description' => $e['DESCRIPTION'],
+			);
+		}
+	}
+	// ici il faut faire attention : un evt apparait N fois
+	// mais on a son id
+	$seen = array();
+	foreach($duree as $amj=>$l){
+		foreach($l as $id=>$e){
+			if (!isset($seen[$e['URL']])){
+				$evt[] = array(
+					'id' => $id,
+					'title' => $e['SUMMARY'],
+					'allDay' => false,
+					'start' => convert_dateical($e['DTSTART']), //Ymd\THis
+					'end' => convert_dateical($e['DTEND']), // Ymd\THis
+					'url' => str_replace('&amp;','&',$e['URL']),
+					'className' => $e['CATEGORIES'],
+					'description' => $e['DESCRIPTION'],
+				);
+				$seen[$e['URL']] = true;
+			}
+		}
+	}
+
+	include_spip('inc/json');
+	echo json_encode($evt);
+}
---
+++ lang/organiseur_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://trac.rezo.net/spip/spip/ecrire/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'cal_par_jour' => 'jour',
+'cal_par_mois' => 'mois',
+'cal_par_semaine' => 'semaine',
+'cal_jour_entier' => 'Journée',
+'loading' => 'Chargement...',
+
+);
