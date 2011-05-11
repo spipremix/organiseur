d1027f715b5975ca76dd2b7912984b2d416e83ce
salvatore <salvatore@rezo.net>
2011-05-11 08:44:37 +0000

    langues (organiseur)

--- lang/organiseur_fr.php
+++ lang/organiseur_fr.php
@@ -1,14 +1,18 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://trac.rezo.net/spip/spip/ecrire/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/organiseur/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'cal_par_jour' => 'jour',
-'cal_par_mois' => 'mois',
-'cal_par_semaine' => 'semaine',
-'cal_jour_entier' => 'Journée',
-'loading' => 'Chargement...',
+	// C
+	'cal_jour_entier' => 'Journée',
+	'cal_par_jour' => 'jour',
+	'cal_par_mois' => 'mois',
+	'cal_par_semaine' => 'semaine',
 
+	// L
+	'loading' => 'Chargement...'
 );
+
+?>
