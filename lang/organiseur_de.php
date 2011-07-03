3449080954e0c2836debeee5fbd308bbe0fbff19
salvatore <salvatore@rezo.net>
2011-07-03 12:36:56 +0000

    langues (organiseur)

---
+++ lang/organiseur_de.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cal_jour_entier' => 'ganztägig',
+	'cal_par_jour' => 'Tag',
+	'cal_par_mois' => 'Monat',
+	'cal_par_semaine' => 'Woche',
+
+	// L
+	'loading' => 'lade ...'
+);
+
+?>
---
+++ lang/organiseur_sk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cal_jour_entier' => 'Deň',
+	'cal_par_jour' => 'deň',
+	'cal_par_mois' => 'mesiac',
+	'cal_par_semaine' => 'týždeň',
+
+	// L
+	'loading' => 'Spúšťa sa...'
+);
+
+?>
