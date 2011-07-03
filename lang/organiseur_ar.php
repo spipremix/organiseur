8754e1e251b72615c2266b1d61cc32c0212fc463
salvatore <salvatore@rezo.net>
2011-07-03 12:36:56 +0000

    langues (organiseur)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/organiseur@49271 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/organiseur_ar.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cal_jour_entier' => 'اليوم',
+	'cal_par_jour' => 'يوم',
+	'cal_par_mois' => 'شهر',
+	'cal_par_semaine' => 'أسبوع',
+
+	// L
+	'loading' => 'تحميل...'
+);
+
+?>
