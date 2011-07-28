e53d316c8642ddc5df6a42783c593ab52a433724
Cerdic <cedric@yterium.com>
2011-07-28 15:46:29 +0000

    debut de boite de reception unifiee (messages, annonces, penses betes)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/organiseur@50047 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/organiseur_fr.php
+++ lang/organiseur_fr.php
@@ -11,8 +11,22 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cal_par_mois' => 'mois',
 	'cal_par_semaine' => 'semaine',
 
+	// I
+	'info_message_de' => 'De',
+	'info_type_message_rv' => 'Rendez-vous',
+	'info_type_message_affich' => 'Annonce',
+	'info_type_message_pb' => 'Pense-bête',
+	'info_type_message_normal' => 'Message',
+	'info_message_non_lu' => 'Nouveau message',
+	'info_1_message_nonlu' => '1 nouveau message',
+	'info_nb_messages_nonlus' => '@nb@ nouveaus messages',
+
 	// L
-	'loading' => 'Chargement...'
+	'loading' => 'Chargement...',
+
+	// T
+	'titre_boite_reception' => 'Boîte de reception',
+	'titre_boite_envoi' => 'Boîte d\'envoi',
 );
 
 ?>
