fa76f0088fc70830d0c2b8f9cef03c515b478b71
salvatore <salvatore@rezo.net>
2011-08-05 14:19:14 +0000

    langues (organiseur)

--- lang/organiseur_fr.php
+++ lang/organiseur_fr.php
@@ -21,58 +21,50 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'icone_ecrire_nouveau_message' => 'Envoyer un nouveau message',
-	'icone_ecrire_nouvelle_annonce' => 'Envoyer une nouvelle annonce',
 	'icone_ecrire_nouveau_pensebete' => 'Ecrire un nouveau pense-bête',
-	'icone_modifier_pensebete' => 'Modifier ce pense-bête',
+	'icone_ecrire_nouvelle_annonce' => 'Envoyer une nouvelle annonce',
 	'icone_modifier_annonce' => 'Modifier cette annonce',
-
-	'info_agenda_interne' => 'Agenda interne',
-	'info_message_de' => 'De',
-	'info_message_a' => 'A',
-	'info_message_objet' => 'Objet',
-	'info_message_date' => 'Date',
-	'info_message_non_lu' => 'Nouveau message',
-
-	'info_selection_annonces' => 'Annonces',
-	'info_selection_pensebetes' => 'Pense-bêtes',
-	'info_selection_messages' => 'Messages',
-
-	'info_type_message_rv' => 'Rendez-vous',
-	'info_type_message_affich' => 'Annonce',
-	'info_type_message_pb' => 'Pense-bête',
-	'info_type_message_normal' => 'Message',
-
-	'info_1_message_nonlu' => '1 nouveau message',
-	'info_nb_messages_nonlus' => '@nb@ nouveaux messages',
+	'icone_modifier_pensebete' => 'Modifier ce pense-bête',
 	'info_1_message_envoye' => '1 message envoyé',
+	'info_1_message_nonlu' => '1 nouveau message',
+	'info_agenda_interne' => 'Agenda interne',
+	'info_message_a' => 'A',
+	'info_message_date' => 'Date',
+	'info_message_de' => 'De',
+	'info_message_non_lu' => 'Nouveau message',
+	'info_message_objet' => 'Objet',
 	'info_nb_messages_envoyes' => '@nb@ messages envoyés',
-
-
+	'info_nb_messages_nonlus' => '@nb@ nouveaux messages',
+	'info_selection_annonces' => 'Annonces',
+	'info_selection_messages' => 'Messages',
+	'info_selection_pensebetes' => 'Pense-bêtes',
+	'info_type_message_affich' => 'Annonce',
+	'info_type_message_normal' => 'Message',
+	'info_type_message_pb' => 'Pense-bête',
+	'info_type_message_rv' => 'Rendez-vous',
 
 	// L
+	'label_destinataires' => 'À',
+	'label_texte' => 'Texte',
+	'label_titre' => 'Sujet',
 	'loading' => 'Chargement...',
-	'label_destinataires' => "À",
-	'label_titre' => "Sujet",
-	'label_texte' => "Texte",
 
 	// M
 	'message' => 'Message',
 	'messages' => 'Messages',
 
 	// N
-	'notification_annonce_publie_1' => '[@nom_site_spip@] Annonce générale',
 	'notification_annonce_lire_a_ladresse' => 'Vous pouvez la lire à l\'adresse suivante @url@.',
-	
+	'notification_annonce_publie_1' => '[@nom_site_spip@] Annonce générale',
+	'notification_message_lire_a_ladresse' => 'Vous pouvez le lire et y répondre à l\'adresse suivante @url@.',
 	'notification_message_publie_1' => '[@nom_site_spip@] Nouveau message',
 	'notification_message_recu_de' => 'Vous avez reçu un nouveau message de la part de @nom@.',
-	'notification_message_lire_a_ladresse' => 'Vous pouvez le lire et y répondre à l\'adresse suivante @url@.',
-
 
 	// T
 	'texte_message_brouillon' => 'Ce message est enregistré comme brouillon',
-	'titre_boite_reception' => 'Boîte de reception',
-	'titre_boite_envoi' => 'Boîte d\'envoi',
 	'titre_agenda_rv' => 'Rendez-vous',
+	'titre_boite_envoi' => 'Boîte d\'envoi',
+	'titre_boite_reception' => 'Boîte de reception'
 );
 
 ?>
--- lang/organiseur_sk.php
+++ lang/organiseur_sk.php
@@ -7,14 +7,66 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// B
+	'bouton_enregistrer_brouillon' => 'Enregistrer en brouillon', # NEW
+	'bouton_envoyer_message' => 'Envoyer', # NEW
+	'bouton_envoyer_message_maintenant' => 'Envoyer maintenant', # NEW
+
 	// C
 	'cal_jour_entier' => 'Deň',
 	'cal_par_jour' => 'deň',
 	'cal_par_mois' => 'mesiac',
 	'cal_par_semaine' => 'týždeň',
 
+	// E
+	'erreur_destinataire_invalide' => 'Le destinataire @dest@ n\'est pas valide', # NEW
+
+	// I
+	'icone_ecrire_nouveau_message' => 'Envoyer un nouveau message', # NEW
+	'icone_ecrire_nouveau_pensebete' => 'Ecrire un nouveau pense-bête', # NEW
+	'icone_ecrire_nouvelle_annonce' => 'Envoyer une nouvelle annonce', # NEW
+	'icone_modifier_annonce' => 'Modifier cette annonce', # NEW
+	'icone_modifier_pensebete' => 'Modifier ce pense-bête', # NEW
+	'info_1_message_envoye' => '1 message envoyé', # NEW
+	'info_1_message_nonlu' => '1 nouveau message', # NEW
+	'info_agenda_interne' => 'Agenda interne', # NEW
+	'info_message_a' => 'A', # NEW
+	'info_message_date' => 'Date', # NEW
+	'info_message_de' => 'De', # NEW
+	'info_message_non_lu' => 'Nouveau message', # NEW
+	'info_message_objet' => 'Objet', # NEW
+	'info_nb_messages_envoyes' => '@nb@ messages envoyés', # NEW
+	'info_nb_messages_nonlus' => '@nb@ nouveaux messages', # NEW
+	'info_selection_annonces' => 'Annonces', # NEW
+	'info_selection_messages' => 'Messages', # NEW
+	'info_selection_pensebetes' => 'Pense-bêtes', # NEW
+	'info_type_message_affich' => 'Annonce', # NEW
+	'info_type_message_normal' => 'Message', # NEW
+	'info_type_message_pb' => 'Pense-bête', # NEW
+	'info_type_message_rv' => 'Rendez-vous', # NEW
+
 	// L
-	'loading' => 'Spúšťa sa...'
+	'label_destinataires' => 'À', # NEW
+	'label_texte' => 'Texte', # NEW
+	'label_titre' => 'Sujet', # NEW
+	'loading' => 'Spúšťa sa...',
+
+	// M
+	'message' => 'Message', # NEW
+	'messages' => 'Messages', # NEW
+
+	// N
+	'notification_annonce_lire_a_ladresse' => 'Vous pouvez la lire à l\'adresse suivante @url@.', # NEW
+	'notification_annonce_publie_1' => '[@nom_site_spip@] Annonce générale', # NEW
+	'notification_message_lire_a_ladresse' => 'Vous pouvez le lire et y répondre à l\'adresse suivante @url@.', # NEW
+	'notification_message_publie_1' => '[@nom_site_spip@] Nouveau message', # NEW
+	'notification_message_recu_de' => 'Vous avez reçu un nouveau message de la part de @nom@.', # NEW
+
+	// T
+	'texte_message_brouillon' => 'Ce message est enregistré comme brouillon', # NEW
+	'titre_agenda_rv' => 'Rendez-vous', # NEW
+	'titre_boite_envoi' => 'Boîte d\'envoi', # NEW
+	'titre_boite_reception' => 'Boîte de reception' # NEW
 );
 
 ?>
