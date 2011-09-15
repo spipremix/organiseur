841c44ddf8c491edd9adec4f29487f74ab47b14b
salvatore <salvatore@rezo.net>
2011-09-15 19:44:32 +0000

    langues (organiseur)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/organiseur@51481 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/organiseur_sk.php
+++ lang/organiseur_sk.php
@@ -8,9 +8,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_enregistrer_brouillon' => 'Enregistrer en brouillon', # NEW
-	'bouton_envoyer_message' => 'Envoyer', # NEW
-	'bouton_envoyer_message_maintenant' => 'Envoyer maintenant', # NEW
+	'bouton_enregistrer_brouillon' => 'Uložiť koncept',
+	'bouton_envoyer_message' => 'Poslať',
+	'bouton_envoyer_message_maintenant' => 'Poslať teraz',
 
 	// C
 	'cal_jour_entier' => 'Deň',
@@ -19,54 +19,56 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'cal_par_semaine' => 'týždeň',
 
 	// E
-	'erreur_destinataire_invalide' => 'Le destinataire @dest@ n\'est pas valide', # NEW
+	'erreur_destinataire_invalide' => 'Príjemca @dest@ nie je platný',
 
 	// I
-	'icone_ecrire_nouveau_message' => 'Envoyer un nouveau message', # NEW
-	'icone_ecrire_nouveau_pensebete' => 'Ecrire un nouveau pense-bête', # NEW
-	'icone_ecrire_nouvelle_annonce' => 'Envoyer une nouvelle annonce', # NEW
-	'icone_modifier_annonce' => 'Modifier cette annonce', # NEW
-	'icone_modifier_pensebete' => 'Modifier ce pense-bête', # NEW
-	'info_1_message_envoye' => '1 message envoyé', # NEW
-	'info_1_message_nonlu' => '1 nouveau message', # NEW
-	'info_agenda_interne' => 'Agenda interne', # NEW
+	'icone_ecrire_nouveau_message' => 'Poslať novú správu',
+	'icone_ecrire_nouveau_pensebete' => 'Vytvoriť novú pripomienku',
+	'icone_ecrire_nouvelle_annonce' => 'Poslať nový oznam',
+	'icone_effacer_message' => 'Effacer ce message', # NEW
+	'icone_modifier_annonce' => 'Upraviť tento oznam',
+	'icone_modifier_pensebete' => 'Upraviť túto pripomienku',
+	'icone_supprimer_message' => 'Supprimer ce message', # NEW
+	'info_1_message_envoye' => '1 odoslaná správa',
+	'info_1_message_nonlu' => '1 nová správa',
+	'info_agenda_interne' => 'Interný kalendár',
 	'info_message_a' => 'A', # NEW
-	'info_message_date' => 'Date', # NEW
-	'info_message_de' => 'De', # NEW
-	'info_message_non_lu' => 'Nouveau message', # NEW
-	'info_message_objet' => 'Objet', # NEW
-	'info_nb_messages_envoyes' => '@nb@ messages envoyés', # NEW
-	'info_nb_messages_nonlus' => '@nb@ nouveaux messages', # NEW
-	'info_selection_annonces' => 'Annonces', # NEW
-	'info_selection_messages' => 'Messages', # NEW
-	'info_selection_pensebetes' => 'Pense-bêtes', # NEW
-	'info_type_message_affich' => 'Annonce', # NEW
-	'info_type_message_normal' => 'Message', # NEW
-	'info_type_message_pb' => 'Pense-bête', # NEW
-	'info_type_message_rv' => 'Rendez-vous', # NEW
+	'info_message_date' => 'Dátum',
+	'info_message_de' => 'Odosielateľ',
+	'info_message_non_lu' => 'Nová správa',
+	'info_message_objet' => 'Objekt',
+	'info_nb_messages_envoyes' => '@nb@ odoslaných správ',
+	'info_nb_messages_nonlus' => '@nb@ nových správ',
+	'info_selection_annonces' => 'Oznamy',
+	'info_selection_messages' => 'Správy',
+	'info_selection_pensebetes' => 'Pripomienky',
+	'info_type_message_affich' => 'Oznam',
+	'info_type_message_normal' => 'Správa',
+	'info_type_message_pb' => 'Pripomienka',
+	'info_type_message_rv' => 'Stretnutie',
 
 	// L
 	'label_destinataires' => 'À', # NEW
-	'label_texte' => 'Texte', # NEW
-	'label_titre' => 'Sujet', # NEW
+	'label_texte' => 'Text',
+	'label_titre' => 'Predmet',
 	'loading' => 'Spúšťa sa...',
 
 	// M
-	'message' => 'Message', # NEW
-	'messages' => 'Messages', # NEW
+	'message' => 'Správa',
+	'messages' => 'Správy',
 
 	// N
-	'notification_annonce_lire_a_ladresse' => 'Vous pouvez la lire à l\'adresse suivante @url@.', # NEW
-	'notification_annonce_publie_1' => '[@nom_site_spip@] Annonce générale', # NEW
-	'notification_message_lire_a_ladresse' => 'Vous pouvez le lire et y répondre à l\'adresse suivante @url@.', # NEW
-	'notification_message_publie_1' => '[@nom_site_spip@] Nouveau message', # NEW
-	'notification_message_recu_de' => 'Vous avez reçu un nouveau message de la part de @nom@.', # NEW
+	'notification_annonce_lire_a_ladresse' => 'To si môžete prečítať na adrese @url@.',
+	'notification_annonce_publie_1' => '[@nom_site_spip@] Všeobecný oznam',
+	'notification_message_lire_a_ladresse' => 'Môžete si to prečítať a odpovedať na to na adrese @url@.',
+	'notification_message_publie_1' => '[@nom_site_spip@] Nová správa',
+	'notification_message_recu_de' => '@nom@ vám poslal(a) novú správu.',
 
 	// T
-	'texte_message_brouillon' => 'Ce message est enregistré comme brouillon', # NEW
-	'titre_agenda_rv' => 'Rendez-vous', # NEW
-	'titre_boite_envoi' => 'Boîte d\'envoi', # NEW
-	'titre_boite_reception' => 'Boîte de reception' # NEW
+	'texte_message_brouillon' => 'Táto správa je uložená ako koncept',
+	'titre_agenda_rv' => 'Stretnutia',
+	'titre_boite_envoi' => 'Na odoslanie',
+	'titre_boite_reception' => 'Doručená pošta'
 );
 
 ?>
