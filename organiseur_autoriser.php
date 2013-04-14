9cba854a3f05dd587bec07b5da7761a5d1f9a169
Bruno Bergot <bruno@eliaz.fr>
2013-04-14 12:08:02 +0000

    http://core.spip.org/issues/2976 un rédacteur ne doit pas pouvroi créer d'annonce
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/organiseur@72034 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- organiseur_autoriser.php
+++ organiseur_autoriser.php
@@ -78,10 +78,11 @@ function autoriser_message_dater_dist($faire, $type='', $id=0, $qui = NULL, $opt
 	return false;
 }
 
-// par defaut, autorisation commune a tous les type de message
-// peut etre surchargee en fonction de $type (pb,affich,normal)
 function autoriser_envoyermessage_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
-	if(!($GLOBALS['meta']['messagerie_agenda'] == 'oui') OR !intval($qui['id_auteur']))
+	if (!($GLOBALS['meta']['messagerie_agenda'] == 'oui') OR !intval($qui['id_auteur']))
+		return false;
+	// on peut envoyer une annonce si on est admin
+	if (!($qui['statut']=='0minirezo') AND $type=='affich')
 		return false;
 	return true;
 }
