75216ec01297aff11e043411a123f3fcbbf039ce
sylvain <sylvain@oliver-fr.org>
2015-03-03 04:33:39 +0000

    [Salvatore] [source:_core_/plugins/organiseur/lang/ organiseur] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/organiseur@87703 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/organiseur_oc_ni_mis.php
@@ -0,0 +1,54 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/organiseur?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_enregistrer_brouillon' => 'Registrà couma un pastrouioun',
+	'bouton_envoyer_message' => 'Mandà',
+	'bouton_envoyer_message_maintenant' => 'Mandà ahura',
+
+	// C
+	'cal_jour_entier' => 'Journada',
+	'cal_par_jour' => 'jou',
+	'cal_par_mois' => 'mès',
+	'cal_par_semaine' => 'semana',
+
+	// E
+	'erreur_date' => 'Aquela data es mau coureta',
+	'erreur_heure' => 'Aquela oura es mau coureta',
+
+	// I
+	'icone_effacer_message' => 'Escassà aqueu message',
+	'icone_modifier_message' => 'Moudificà aqueu message',
+	'icone_supprimer_message' => 'Suprimà aqueu message',
+	'info_1_message_envoye' => '1 message mandat',
+	'info_1_message_nonlu' => '1 message nouvèu',
+	'info_message_a' => 'A',
+	'info_message_date' => 'Data',
+	'info_message_de' => 'Da',
+	'info_message_objet' => 'Ouget',
+	'info_nb_messages_envoyes' => '@nb@ message mandat',
+	'info_nb_messages_nonlus' => '@nb@ message nouvèu',
+	'info_selection_messages' => 'Message',
+	'info_type_message_normal' => 'Message',
+	'info_type_message_rv' => 'Apountamen',
+
+	// L
+	'label_destinataires' => 'A',
+	'label_texte' => 'Tèst',
+	'label_titre' => 'Soujet',
+
+	// M
+	'message' => 'Message',
+	'messages' => 'Message',
+
+	// T
+	'titre_agenda_rv' => 'Apountamen'
+);
+
+?>
