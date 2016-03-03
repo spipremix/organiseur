7d1dcde4b0623847ced104f8235e243e66bc14e5
spip <spip@allesfrans.com>
2016-03-03 04:35:52 +0000

    [Salvatore] [source:_core_/plugins/organiseur/lang/ organiseur] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/organiseur@95722 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/organiseur_nl.php
+++ lang/organiseur_nl.php
@@ -22,21 +22,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'erreur_date' => 'De datum is ongeldig',
-	'erreur_date_avant_apres' => 'Geef een einddatum na de begindatum aan !',
+	'erreur_date_avant_apres' => 'Geef een einddatum na de begindatum aan!',
 	'erreur_date_corrigee' => 'De datum werd verbeterd',
 	'erreur_destinataire_invalide' => 'De ontvanger @dest@ is niet geldig',
 	'erreur_heure' => 'Tijdstip is ongeldig',
 	'erreur_heure_corrigee' => 'Het tijdstip werd verbeterd',
 
 	// I
-	'icone_ecrire_nouveau_message' => 'Een nieuwe boodschap opsturen',
+	'icone_ecrire_nouveau_message' => 'Een nieuwe bericht opsturen',
 	'icone_ecrire_nouveau_pensebete' => 'Een herinneringsmemo aanmaken',
 	'icone_ecrire_nouvelle_annonce' => 'Een nieuwe aankondiging aanmaken',
-	'icone_effacer_message' => 'Deze boodschap uitwissen',
+	'icone_effacer_message' => 'Dit bericht wissen',
 	'icone_modifier_annonce' => 'Deze aankondiging veranderen',
 	'icone_modifier_message' => 'Dit bericht aanpassen',
 	'icone_modifier_pensebete' => 'De herinneringsmemo veranderen',
-	'icone_supprimer_message' => 'Deze boodschap uitwissen',
+	'icone_supprimer_message' => 'Dit bericht wissen',
 	'info_1_message_envoye' => '1 boodschap opgestuurd',
 	'info_1_message_nonlu' => '1 nieuwe boodschap',
 	'info_agenda_interne' => 'Interne agenda',
@@ -45,7 +45,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_message_date_debut' => 'Begindatum',
 	'info_message_date_fin' => 'Einddatum',
 	'info_message_de' => 'Van',
-	'info_message_non_lu' => 'Nieuwe boodschap',
+	'info_message_non_lu' => 'Nieuwe bericht',
 	'info_message_objet' => 'Onderwerp',
 	'info_nb_messages_envoyes' => '@nb@ boodschapppen opgestuurd',
 	'info_nb_messages_nonlus' => '@nb@ nieuwe boodschappen',
@@ -53,7 +53,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_selection_messages' => 'Boodschappen',
 	'info_selection_pensebetes' => 'Herinneringsmemo’s',
 	'info_type_message_affich' => 'Aankondiging',
-	'info_type_message_normal' => 'Boodschap',
+	'info_type_message_normal' => 'Bericht',
 	'info_type_message_pb' => 'Herinneringsmemo',
 	'info_type_message_rv' => 'Afspraak',
 
@@ -64,18 +64,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'loading' => 'Aan het laden...',
 
 	// M
-	'message' => 'Boodschap',
-	'messages' => 'Boodschappen',
+	'message' => 'Bericht',
+	'messages' => 'Berichten',
 
 	// N
 	'notification_annonce_lire_a_ladresse' => 'Je kan ze lezen op het volgende addres: @url@.',
 	'notification_annonce_publie_1' => '[@nom_site_spip@] Algemene aankondiging',
-	'notification_message_lire_a_ladresse' => 'Je kan de boodschap lezen en beantwoorden op het adres: @url@.',
-	'notification_message_publie_1' => '[@nom_site_spip@] Nieuwe boodschap',
-	'notification_message_recu_de' => 'Je hebt een nieuwe boodschap gekregen van @nom@.',
+	'notification_message_lire_a_ladresse' => 'Je kan het bericht lezen en beantwoorden op het adres: @url@.',
+	'notification_message_publie_1' => '[@nom_site_spip@] Nieuw bericht',
+	'notification_message_recu_de' => 'Je hebt een nieuwe bericht gekregen van @nom@.',
 
 	// T
-	'texte_message_brouillon' => 'Deze boodschap werd als klad opgeslagen.',
+	'texte_message_brouillon' => 'Deze bericht werd als klad opgeslagen.',
 	'titre_agenda_rv' => 'Afspraak',
 	'titre_boite_envoi' => 'Outbox',
 	'titre_boite_reception' => 'INBOX'
