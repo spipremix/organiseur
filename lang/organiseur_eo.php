c25fb8883e6802374c05a303c257d7428f002f5e
gpl <gpl@long-player.net>
2018-03-06 04:36:37 +0000

    [Salvatore] [source:_core_/plugins/organiseur/lang/ organiseur] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/organiseur_eo.php
@@ -0,0 +1,82 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/organiseur?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_enregistrer_brouillon' => 'Konservi kiel malneto',
+	'bouton_envoyer_message' => 'Sendi',
+	'bouton_envoyer_message_maintenant' => 'Tuj sendi',
+
+	// C
+	'cal_jour_entier' => 'Tago',
+	'cal_par_jour' => 'tago',
+	'cal_par_mois' => 'monato',
+	'cal_par_semaine' => 'semajno',
+
+	// E
+	'erreur_date' => 'Tiu dato estas erara',
+	'erreur_date_avant_apres' => 'Findato estu posta al la komencodato !',
+	'erreur_date_corrigee' => 'La dato estis korektita.',
+	'erreur_destinataire_invalide' => 'La adresato @dest@ ne validas',
+	'erreur_heure' => 'Tiu horo ne validas',
+	'erreur_heure_corrigee' => 'La horo estis korektita.',
+
+	// I
+	'icone_ecrire_nouveau_message' => 'Sendi novan mesaĝon',
+	'icone_ecrire_nouveau_pensebete' => 'Skribi novan memorigilon',
+	'icone_ecrire_nouvelle_annonce' => 'Sendi novan avizon',
+	'icone_effacer_message' => 'Forigi tiun mesaĝon',
+	'icone_modifier_annonce' => 'Modifi tiun avizon',
+	'icone_modifier_message' => 'Modifi tiun mesaĝon',
+	'icone_modifier_pensebete' => 'Modifi tiun memorigilon',
+	'icone_supprimer_message' => 'Forigi tiun mesaĝon',
+	'info_1_message_envoye' => '1 mesaĝo sendita',
+	'info_1_message_nonlu' => '1 nova mesaĝo',
+	'info_agenda_interne' => 'Interna agendo',
+	'info_message_a' => 'Al',
+	'info_message_date' => 'Dato',
+	'info_message_date_debut' => 'Komencodato',
+	'info_message_date_fin' => 'Findato',
+	'info_message_de' => 'De',
+	'info_message_non_lu' => 'Nova mesaĝo',
+	'info_message_objet' => 'Pri',
+	'info_nb_messages_envoyes' => '@nb@ mesaĝoj senditaj',
+	'info_nb_messages_nonlus' => '@nb@ novaj mesaĝoj',
+	'info_selection_annonces' => 'Avizoj',
+	'info_selection_messages' => 'Mesaĝoj',
+	'info_selection_pensebetes' => 'Memorigiloj',
+	'info_type_message_affich' => 'Avizo',
+	'info_type_message_normal' => 'Mesaĝo',
+	'info_type_message_pb' => 'Memorigilo',
+	'info_type_message_rv' => 'Rendevuo',
+
+	// L
+	'label_destinataires' => 'Al',
+	'label_texte' => 'Teksto',
+	'label_titre' => 'Pri',
+	'loading' => 'Ŝargante...',
+
+	// M
+	'message' => 'Mesaĝo',
+	'messages' => 'Mesaĝoj',
+
+	// N
+	'notification_annonce_lire_a_ladresse' => 'Vi povas legi ĝin tie : @url@.',
+	'notification_annonce_publie_1' => '[@nom_site_spip@] Ĝenerala avizo',
+	'notification_message_lire_a_ladresse' => 'Vi povas legi ĝin kaj respondi al ĝi tie : @url@.',
+	'notification_message_publie_1' => '[@nom_site_spip@] Nova mesaĝo',
+	'notification_message_recu_de' => 'Vi ricevis novan mesaĝon de @nom@.',
+
+	// T
+	'texte_message_brouillon' => 'Tiu mesaĝo estas konservita kiel malneto',
+	'titre_agenda_rv' => 'Rendevuo',
+	'titre_boite_envoi' => 'Senditujo',
+	'titre_boite_reception' => 'Ricevujo'
+);
